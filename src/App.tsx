77ffa9a9a972e56189440e452b1d58f05063c97d
Biren Saud <brensaud4@gmail.com>
2021-06-20 14:20:23 +0545

    relocated components, logged in user can only acccess secure components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./App.css";
 
-import Nav from './components/Nav';
+import Nav from './secure/components/Nav';
 
-import Menu from './components/Menu';
+import Menu from './secure/components/Menu';
 
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./secure/Dashboard";
+import Users from "./secure/components/Users";
 
 function App() {
   return (
@@ -18,7 +22,10 @@ function App() {
           <Menu />
 
           <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-            <Dashboard />
+            <BrowserRouter>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/users" component={Users} />
+            </BrowserRouter>
           </main>
         </div>
       </div>
