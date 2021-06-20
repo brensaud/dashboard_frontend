97c8d4ee9d25554378a110011b5d6607fe58f5c3
Biren Saud <brensaud4@gmail.com>
2021-06-20 11:37:34 +0545

    Menu and Dashboard component created

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,12 +6,7 @@ const Nav = () => {
       <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
         Company name
       </a>
-      <input
-        className="form-control form-control-dark w-100"
-        type="text"
-        placeholder="Search"
-        aria-label="Search"
-      />
+
       <ul className="navbar-nav px-3">
         <li className="nav-item text-nowrap">
           <a className="nav-link" href="#">
