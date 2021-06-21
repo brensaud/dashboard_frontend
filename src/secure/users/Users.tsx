5885911127bac12bbc4c236522d9964935482817
Biren Saud <brensaud4@gmail.com>
2021-06-21 11:00:33 +0545

    role and action are implemented

--- src/secure/users/Users.tsx
+++ src/secure/users/Users.tsx
@@ -23,6 +23,14 @@ export default class Users extends Component {
     render() {
         return (
             <Wrapper>
+
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+                <div className="btn-toolbar mb-2 mb-md-0">
+                    <a href="" className="btn btn-sm btn-outline-secondary">Add</a>
+                </div>
+            </div>
+
+
             <div className="table-responsive">
               <table className="table table-striped table-sm">
                 <thead>
@@ -41,9 +49,14 @@ export default class Users extends Component {
                             <tr key={user.id}>
                                 <td>{user.id}</td>
                                 <td>{user.first_name} {user.last_name}</td>
-                                <td>user.email</td>
-                                <td>Empty</td>
-                                <td>Empty</td>
+                                <td>{user.email}</td>
+                                <td>{user.role.name}</td>
+                                <td>
+                                    <div className="btn-group mr-2">
+                                        <a href="" className="btn btn-sm btn-outline-secondary">Edit</a>
+                                        <a href="" className="btn btn-sm btn-outline-secondary">Delete</a>
+                                    </div>
+                                </td>
                             </tr>
                         )
                     )}
